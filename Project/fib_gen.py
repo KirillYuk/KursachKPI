0e14a211bb560a953b8d4417ae9286a3a16e856f
KirillYuk <qwertyunisky@gmail.com>
2026-02-15 14:42:43 +0200

    wrap in func

--- Project/fib_gen.py
+++ Project/fib_gen.py
@@ -1,10 +1,13 @@
 import time
-a = 0
-b = 1
 
-while True:
-    print(a)
-    temp = a
-    a = b
-    b = temp + a
-    time.sleep(0.5)
+def fib_gen():
+    a = 0
+    b = 1
+    while True:
+        print(a)
+        temp = a
+        a = b
+        b = temp + a
+        time.sleep(0.5)
+        
+fib_gen()
