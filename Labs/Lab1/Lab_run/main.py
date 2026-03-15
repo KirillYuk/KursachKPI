19a52077e8fc3d4393f5db6428ab2375da8285a8
KirillYuk <qwertyunisky@gmail.com>
2026-03-15 18:48:03 +0200

    lib created

--- Labs/Lab1/Lab_run/main.py
+++ Labs/Lab1/Lab_run/main.py
@@ -1,14 +1,14 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(""))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from fib_gen_lib.generator import fib_gen, run
 
 if __name__ == "__main__":
     try:
-                
         sec = int(input("How many seconds work?: "))
         fib_iter = fib_gen()
         run(fib_iter, sec)
-
     except ValueError:
-        print("ValueError")
+        print("Please enter a valid number.")
