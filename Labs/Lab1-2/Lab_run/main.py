1ee54df78d0b6a045c9353ccd8ea9111e83805d1
KirillYuk <qwertyunisky@gmail.com>
2026-03-22 19:56:14 +0200

    changed lib import logic

--- Labs/Lab1-2/Lab_run/main.py
+++ Labs/Lab1-2/Lab_run/main.py
@@ -1,9 +1,4 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from fib_gen_lib.generator import fib_gen, run
+from generator import fib_gen, run
 
 if __name__ == "__main__":
     try:
