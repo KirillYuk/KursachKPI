cb7b653a09d94ba3e6d4c7e21983d3a826c71c8d
KirillYuk <qwertyunisky@gmail.com>
2026-04-10 13:51:02 +0300

    add dequeue (highest/lowest)

--- Labs/Lab4/queue.py
+++ Labs/Lab4/queue.py
@@ -6,3 +6,12 @@ class BiQueue:
     def enqueue(self, item, priority):
         self.queue.append((self.counter, priority,item))
         self.counter +=1
+
+    def dequeue(self, mode):
+        if mode == 'highest':
+            item = max(self.queue, key=lambda x: x[1])
+        elif mode == 'lowest':
+            item = min(self.queue, key=lambda x: x[1])
+
+        self.queue.remove(item)
+        return item[2]
