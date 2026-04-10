9f531dc2e10d830fdc29035d8315f95543f779ee
KirillYuk <qwertyunisky@gmail.com>
2026-04-10 13:00:41 +0300

    implement enqueue

--- Labs/Lab4/main.py
+++ Labs/Lab4/main.py
@@ -1,4 +1,7 @@
 from queue import  BiQueue
 
 q = BiQueue()
+q.enqueue("A", 5)
+q.enqueue("B", 1)
+q.enqueue("C", 3)
 print(q.queue)
--- Labs/Lab4/queue.py
+++ Labs/Lab4/queue.py
@@ -2,3 +2,7 @@ class BiQueue:
     def __init__(self):
         self.queue = []
         self.counter = 0
+    
+    def enqueue(self, item, priority):
+        self.queue.append((self.counter, priority,item))
+        self.counter +=1
